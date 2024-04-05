7c10ea48858b7a9a1bd089b009014e62fb17cd68
Rafau <rafalkajca11@gmail.com>
2024-04-05 15:25:47 +0200

    aaaaa

--- ViewModels/FridgeProductViewModel.cs
+++ ViewModels/FridgeProductViewModel.cs
@@ -3,6 +3,7 @@ namespace Server.ViewModels;
 public class FridgeProductViewModel 
 {
     public int ProductId { get; set; }
+    
     public string Login { get; set; }
 
     public double Quantity { get; set; }
